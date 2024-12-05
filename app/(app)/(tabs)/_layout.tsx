6271a5e0d8ccd2238c4c2b5a2d0f212686ff65fb
Phoenix <abinathapleslie@gmail.com>
2024-12-05 19:39:05 +0530

    feat: added account

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -55,6 +55,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(app)/(tabs)/account.tsx
@@ -0,0 +1,155 @@
+import { Colors } from "@/constants/Colors";
+import { SIZES } from "@/constants/Sizes";
+import { StyleSheet, Image, View } from "react-native";
+import { Avatar, TextInput, Text, Button } from "react-native-paper";
+
+export default function Account() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("@/assets/images/banner.png")}
+        style={{
+          width: "100%",
+          height: SIZES.height * 0.18,
+          resizeMode: "cover",
+        }}
+      />
+      <View style={styles.contentContainer}>
+        <View style={styles.avtar}>
+          <Avatar.Image
+            size={SIZES.width * 0.3}
+            source={require("@/assets/images/icon.png")}
+          />
+        </View>
+        <View style={styles.form}>
+          <View>
+            <Text variant="labelLarge" style={{ marginBottom: 4 }}>
+              Name
+            </Text>
+
+            <TextInput
+              mode="flat"
+              label=""
+              value="Ricard Steven"
+              theme={{
+                colors: { surfaceVariant: Colors["light"].background },
+                roundness: 8,
+              }}
+              underlineColor="transparent"
+              activeUnderlineColor="transparent"
+              style={{
+                borderRadius: 8,
+                overflow: "hidden",
+              }}
+              right={
+                <TextInput.Icon
+                  icon="account-outline"
+                  size={16}
+                  style={{
+                    backgroundColor: Colors["light"].secondary,
+                    borderRadius: 8,
+                  }}
+                />
+              }
+            />
+          </View>
+
+          <View>
+            <Text variant="labelLarge" style={{ marginBottom: 4 }}>
+              Phone number
+            </Text>
+
+            <TextInput
+              mode="flat"
+              label=""
+              value="+1234567890"
+              theme={{
+                colors: { surfaceVariant: Colors["light"].background },
+                roundness: 8,
+              }}
+              underlineColor="transparent"
+              activeUnderlineColor="transparent"
+              style={{
+                borderRadius: 8,
+                overflow: "hidden",
+              }}
+              right={
+                <TextInput.Icon
+                  icon="eye"
+                  size={16}
+                  style={{
+                    backgroundColor: Colors["light"].secondary,
+                    borderRadius: 8,
+                  }}
+                />
+              }
+            />
+          </View>
+
+          <View>
+            <Text variant="labelLarge" style={{ marginBottom: 4 }}>
+              Email
+            </Text>
+
+            <TextInput
+              mode="flat"
+              label=""
+              value="ricardsteven90@gmail.com"
+              theme={{
+                colors: { surfaceVariant: Colors["light"].background },
+                roundness: 8,
+              }}
+              underlineColor="transparent"
+              activeUnderlineColor="transparent"
+              style={{
+                borderRadius: 8,
+                overflow: "hidden",
+              }}
+              right={
+                <TextInput.Icon
+                  icon="email-outline"
+                  size={16}
+                  style={{
+                    backgroundColor: Colors["light"].secondary,
+                    borderRadius: 8,
+                  }}
+                />
+              }
+            />
+          </View>
+
+          <Button
+            mode="contained"
+            theme={{roundness:0}}
+            style={{borderRadius:8}}
+            dark
+            buttonColor={"black"}
+            textColor="white"
+            contentStyle={{ height: 50}}
+          >
+            Save
+          </Button>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors["light"].secondary,
+  },
+  contentContainer: {
+    flex: 1,
+    marginHorizontal: 24,
+  },
+  avtar: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 24,
+  },
+  form: {
+    gap: 16,
+  },
+});
